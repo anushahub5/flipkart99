86b83849b0a1954e1e81c2d88f6638fff12e99b0
anushahub5 <anusharaju555@gmail.com>
2020-04-04 12:29:07 +0530

    changed dev-1.2.5 code

--- loginuser.java
+++ loginuser.java
@@ -2,9 +2,9 @@ public class login
 {
         public static void main(String args[])
         {
-		int a=150;
+		int a=200;
                 int i;
-		int a=150;
+		int a=200;
 
                 for(i=0;i<=10;i++)
                
