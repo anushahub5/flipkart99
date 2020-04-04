8c685fe8f8b92e72ba87b67326b6cd6f41fe9e0d
anushahub5 <anusharaju555@gmail.com>
2020-04-04 11:41:07 +0530

    changed a=10

--- loginuser.java
+++ loginuser.java
@@ -3,6 +3,7 @@ public class login
         public static void main(String args[])
         {
                 int i;
+		int a=10;
                 for(i=0;i<=10;i++)
                 {
                 }
